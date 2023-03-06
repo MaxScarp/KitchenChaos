33f1fedc6e95b0dcd2457d9e5a4e7ee950c688bc
MaxScarp <massy1997.lavoro@gmail.com>
2023-03-06 20:53:17 +0100

    StoveCounter logics
    Created the StoveCounter using a FiniteStateMachine.

---
+++ Assets/Scripts/ScriptableObjects/BurningRecipeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/BurningRecipe")]
+public class BurningRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+    public float burningTimerMax;
+}
---
+++ Assets/Scripts/ScriptableObjects/FryingRecipeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/FryingRecipe")]
+public class FryingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+    public float fryingTimerMax;
+}
